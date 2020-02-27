fff6ff3cc9487ba71b3a652944635d784496acb1
Eric Carlson <ericc254@yahoo.com>
2020-02-27 12:48:09 -0600

    Moved random article button

--- dist/js/main.js
+++ dist/js/main.js
@@ -10621,31 +10621,48 @@ var apod = {
       return `${y}-${m}-${d}`;
     },
 
-    // Application Constructor
-    init: function() {
-        let date = "2013-06-06";
-        //let date = this.randomDate(new Date(1995, 5, 16), new Date());
-        var url = "https://api.nasa.gov/planetary/apod?api_key=X1krhYpWPsva9bhvIRZVDcb0XSiBB5uU6l5JZHuw&date=" + date;
+  //Injects the results of the API call into the DOM
+    buildDOM: function(result) {
+    $("#apodTitle").text(result.title);
+  
+    if(result.media_type === 'video') {
+      $("#apodImage").hide();
+      $("#apodVideo > iframe").attr("src", result.url).show();
+    }else{
+      $("#apodVideo").hide();
+      $("#apodImg").attr("src", result.url).attr('alt', result.title).show();
+    }
+  
+    $("#apodCopyright").text("Copyright: " + result.copyright);
+    $("#apodDate").text("Date: " + result.date);
+    $("#apodDesc").text(result.explanation);
+  },
+  
+  //Executes an AJAX call to an API.
+  getRequest: function() {
+    let _this = this;
+    let date = this.randomDate(new Date(1995, 5, 16), new Date());
+    let url = "https://api.nasa.gov/planetary/apod?api_key=X1krhYpWPsva9bhvIRZVDcb0XSiBB5uU6l5JZHuw&date=" + date;
+    $.ajax({
+        url: url
+    }).done(function(result){
+        _this.buildDOM(result);
+    }).fail(function(result){
+      console.log(result);
+    });
+  },
+  
+  // Initialization method.
+  init: function() {
+    this.getRequest();
+  },
+}
 
-        $.ajax({
-            url: url
-        }).done(function(result){
-            $("#apodTitle").text(result.title);
-            //If the media type is video hide the image elements and display a video.
-            if(result.media_type === 'video') {
-                $("#apodImg").hide();
-                $("#apodVideo > iframe").attr("src", result.url).show();
-            }else{
-                $("#apodVideo").hide();
-                $("#apodImg").attr("src", result.url).attr('alt', result.title).show();
-            }
-            $("#apodImg").attr("src", result.url).attr('alt', result.title);
-            $("#apodCopyright").text("Copyright: " + result.copyright);
-            $("#apodDate").text("Date: " + date);
-            $("#apodDesc").text(result.explanation);
-          }).fail(function(result){
-          console.log(result);
-        });
-    },
-};
 apod.init();
+
+/* https://learn.jquery.com/using-jquery-core/document-ready/ */
+$(function() {
+    $('#btnRandApod').on('click',function(){
+      apod.getRequest();
+    });
+});
